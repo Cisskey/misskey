563612095abd97d25bf73fc170a2108097d85113
takonomura <takonomura@users.noreply.github.com>
2020-10-18 14:59:08 +0900

    Fix prism theme

--- src/client/components/mfm.ts
+++ src/client/components/mfm.ts
@@ -11,7 +11,7 @@ import MkCode from './code.vue';
 import MkGoogle from './google.vue';
 import { host } from '@/config';
 import { RouterLink } from 'vue-router';
-import 'prismjs/themes/prism.css';
+import 'prismjs/themes/prism-okaidia.css';
 
 function toDirection(attr: string): string {
 	switch (attr) {
