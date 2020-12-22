21055c149c4dcf2475e694d3fc0d18547e71c7e1
takonomura <takonomura@users.noreply.github.com>
2020-12-23 01:52:36 +0900

    Fix i18n

--- src/client/i18n.ts
+++ src/client/i18n.ts
@@ -30,6 +30,7 @@ export const locale = await count(clientDb.i18n).then(async n => {
 });
 
 export const i18n = createI18n({
+	globalInjection: true,
 	sync: false,
 	locale: _lang,
 	messages: { [_lang]: locale }
